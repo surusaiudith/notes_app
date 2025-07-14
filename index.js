5b5582fef58525c48f67f1b15be6332c47ccf93a
suru sai udith <saiudith1138@gmail.com>
2025-07-14 12:23:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express=require('express');//to use express
+const fs=require('fs');//builtin file system module
+const path=require('path');
+const NOTES_FILE =path.join(__dirname,'notes.json');//makes sure that file path works on all systems
 const app=express();//to get on instance of express
 const PORT=3000;//the port in which the app is launched
 app.use(express.json());//to use built in middleware in express
 
 let notes=[];//to store notes resets after restarting
+if(fs.existsSync(NOTES_FILE)){//if notes.json exists the notes are synced
+    const data=fs.readFileSync(NOTES_FILE,'utf-8');
+    notes=JSON.parse(data);//converts json string to js array and store it in notes
+}
+function saveNotesToFile(){
+    fs.writeFileSync(NOTES_FILE,JSON.stringify(notes,null,2));//to save notes to notes.json
+}
 
 app.get('/',(req,res)=>{//home route
     res.send('welcome to notes app');//sends a respose to whoever visits port 3000
@@ -19,8 +29,9 @@ app.post('/notes',(req,res)=>{//to post a new note
         id:Date.now(),//creates unique id of the note based on date
         text:text
     }
-
+    
     notes.push(newnote);//to add new note into array
+    saveNotesToFile();
     res.status(201).json(newnote);//alerts by sending created status
 });
 
@@ -37,6 +48,7 @@ app.delete('/notes/:id',(req,res)=>{
     }
 
     const deletedNote=notes.splice(index,1)[0];//deletes the note from array and returns the deleted note
+    saveNotesToFile();
     res.json(deletedNote);
 });
 
